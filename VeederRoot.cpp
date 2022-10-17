7dfea35f839c9a67927abbd721f82a4aab8b79e0
Nitin Sharma <nitin@fuelbuddy.in>
2022-10-17 14:14:30 +0530

    NO_T fixing release.
    Signed-off-by: Nitin Sharma <nitin@fuelbuddy.in>

--- VeederRoot.cpp
+++ VeederRoot.cpp
@@ -5,3 +5,12 @@
 VeederRoot::VeederRoot(int buad_rate=9600, int pin_rx=17, int pin_tx=16) {
   Dispencer::Dispencer(int buadRate, int pinRx, int pinTx);
 }
+
+VeederRoot::readDispencerStatus() {
+  return 0;
+}
+
+
+VeederRoot::readTotalizer() {
+  return 0;
+}
