4a4355cc47519a491c039b7e163e0f7575191967
Sunny Sahu <sunny.sahu@fuelbuddy.in>
2022-10-18 14:26:17 +0530

    NO_T fixing build cycle
    Signed-off-by: Sunny Sahu <sunny.sahu@fuelbuddy.in>

--- Dispencer.h
+++ Dispencer.h
@@ -2,17 +2,17 @@
 #define DISPENCER_H
 
 #if (ARDUINO > 100)
-  #include <Arduino.h>
+#include <Arduino.h>
 #else
-  #include <WProgram.h>
+#include <WProgram.h>
 #endif
 
 #include <HardwareSerial.h>
 
-class Dispencer 
+class Dispencer
 {
 public:
-  Dispencer(int buad_rate=9600, int pin_rx=17, int pin_tx=16);
+  Dispencer(int buad_rate = 9600, int pin_rx = 17, int pin_tx = 16);
   /**
    * connect dispencer on serial port
    */
@@ -28,77 +28,89 @@ public:
   /**
    * status poll
    */
-  virtual int readDispencerStatus(){
+  virtual int readDispencerStatus()
+  {
     return 0;
-    }
+  }
   /**
    * read totalizer reading
    */
-  virtual int readTotalizer(){
+  virtual int readTotalizer()
+  {
     return 0;
-    }
+  }
   /**
    * authorize the sale and start dispencing
    */
-  virtual int authorizeSale(){
+  virtual int authorizeSale()
+  {
     return 0;
-    }
+  }
   /**
    * sets the quantity to be dispencend
    * [dispencer specific]
    */
-  virtual int sendPreset(){
+  virtual int sendPreset()
+  {
     return 0;
-    }
+  }
   /**
    * suspendDispencer, clearSale, pumpStop
    */
-  virtual int stopDispencer(){
+  virtual int stopDispencer()
+  {
     return 0;
-    }
+  }
   /**
    * suspend Dispencer
    */
-  virtual int suspendDispencer(){
+  virtual int suspendDispencer()
+  {
     return 0;
-    }
+  }
   /**
    * resume the dispencer
    */
-  virtual int resumeDispencer(){
+  virtual int resumeDispencer()
+  {
     return 0;
-    }
+  }
   /**
    * clear sale
    */
-  virtual int clearSale(){
+  virtual int clearSale()
+  {
     return 0;
-    }
+  }
   /**
    * read sale
    */
-  virtual int readSale(){
+  virtual int readSale()
+  {
     return 0;
-    }
+  }
   /**
    * stop the external pump
    */
-  virtual int pumpStop(){
+  virtual int pumpStop()
+  {
     return 0;
-    }
+  }
   /**
    * start the external pump
    */
-  virtual int pumpStart(){
+  virtual int pumpStart()
+  {
     return 0;
-    }
+  }
   /**
    * switch dispencer mode to online.
    */
-  virtual int switchMode(bool online=true){
+  virtual int switchMode(bool online = true)
+  {
     return 0;
-    }
-  
+  }
+
   /**
    * read the value from serialdata
    */
@@ -108,25 +120,26 @@ public:
    * serial Event manager
    */
   void serialEvent();
-  
+
   /**
    * data has been fetched
    */
   bool isReadyToRead();
-  
-  
+
 protected:
   HardwareSerial dispencerSerial = HardwareSerial(2);
   int baud_rate;
   int pin_rx;
   int pin_tx;
   String serial_data;
-  bool is_ready_to_read=false;
-  byte * presetCalculate(float quantity);
-  virtual int internalPumpStart() {
+  bool is_ready_to_read = false;
+  byte *presetCalculate(float quantity);
+  virtual int internalPumpStart()
+  {
     return 0;
   };
-  virtual int internalPumpStop() {
+  virtual int internalPumpStop()
+  {
     return 0;
   };
 };
