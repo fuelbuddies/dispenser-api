1feb744f5875ab11e366a35af78302e73b103fb7
Nitin Sharma <nitin@fuelbuddy.in>
2023-09-07 10:29:11 +0530

    NO_T fixing release
    Signed-off-by: Nitin Sharma <nitin@fuelbuddy.in>

--- src/VeederEmr4.cpp
+++ src/VeederEmr4.cpp
@@ -141,9 +141,3 @@ int VeederEmr4::cancelPreset()
 {
   return 0;
 }
-
-int VeederEmr4::sendPreset(float quantity)
-{
-  // Provide the implementation for VeederEmr4 specific behavior here
-  return 0; // Return an appropriate value or perform the necessary actions
-}
