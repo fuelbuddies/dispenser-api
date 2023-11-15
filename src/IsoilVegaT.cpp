ceadb5b80d97f33ce122b5349263acebfc7f6ea9
Nitin Sharma <nitin@fuelbuddy.in>
2023-11-15 18:06:10 +0400

    NO_T fixing cancelPreset isoil
    Signed-off-by: Nitin Sharma <nitin@fuelbuddy.in>

--- src/IsoilVegaT.cpp
+++ src/IsoilVegaT.cpp
@@ -67,7 +67,7 @@ int IsoilVegaT::switchMode(bool online) {
 }
 
 int IsoilVegaT::cancelPreset() {
-    this->sendPreset(0f);
+    return this->sendPreset(0f);
 }
 
 int IsoilVegaT::readDispencerStatus() {
